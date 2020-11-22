cf932af740d0980c59cb42988b5d20936f2a2774
Dipenkumar2293 <71035170+Dipenkumar2293@users.noreply.github.com>
2020-11-22 08:57:05 -0800

    update with Database with Updated RexycleView

--- app/src/main/java/com/dipen/testprojectapp/DailyListFragment.java
+++ app/src/main/java/com/dipen/testprojectapp/DailyListFragment.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.dipen.testprojectapp.dummy.DummyContent;
 
 import java.util.List;
 
@@ -26,10 +25,9 @@ import java.util.List;
 public class DailyListFragment extends Fragment {
 
      private EventsViewModel eventsViewModel;
+
     // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
+
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -43,7 +41,7 @@ public class DailyListFragment extends Fragment {
     public static DailyListFragment newInstance(int columnCount) {
         DailyListFragment fragment = new DailyListFragment();
         Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
+       // args.putInt(ARG_COLUMN_COUNT, columnCount);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,34 +49,28 @@ public class DailyListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_daily_list_list, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+
+       final EventsAdapter adapter = new EventsAdapter();
+        recyclerView.setAdapter(adapter);
         eventsViewModel = new ViewModelProvider(this).get(EventsViewModel.class);
         eventsViewModel.getAllEvents().observe(this, new Observer<List<Events>>() {
             @Override
             public void onChanged(List<Events> events) {
-                Toast.makeText(getContext(),"Hi Guys!!", Toast.LENGTH_SHORT).show();
+                adapter.setEventsList(events);
+                //Toast.makeText(getContext(),"Hi Guys!!", Toast.LENGTH_SHORT).show();
             }
         });
-        View view = inflater.inflate(R.layout.fragment_daily_list_list, container, false);
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS));
-        }
         return view;
     }
 }
---
+++ app/src/main/java/com/dipen/testprojectapp/EventsAdapter.java
@@ -0,0 +1,61 @@
+package com.dipen.testprojectapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventHolder> {
+
+    private List<Events> eventsList =  new ArrayList<>();
+
+    @NonNull
+    @Override
+    public EventHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_daily_list,
+                parent,false);
+        return new EventHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventHolder holder, int position) {
+        Events currentEvent = eventsList.get(position);
+        holder.sTime.setText(currentEvent.getStartTime());
+        holder.eTime.setText(currentEvent.getEndTime());
+        holder.eventText.setText(currentEvent.getName());
+        holder.dateText.setText(currentEvent.getDate());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventsList.size();
+    }
+
+    public void setEventsList(List<Events> eventsList) {
+        this.eventsList = eventsList;
+        notifyDataSetChanged();
+    }
+
+    class EventHolder extends RecyclerView.ViewHolder{
+        private TextView sTime;
+        private TextView eTime;
+        private TextView dateText;
+        private TextView eventText;
+
+
+        public EventHolder(@NonNull View itemView) {
+            super(itemView);
+            sTime = itemView.findViewById(R.id.startTimer);
+            eTime = itemView.findViewById(R.id.endTimer);
+            eventText = itemView.findViewById(R.id.event_Content);
+            dateText = itemView.findViewById(R.id.dateShow);
+        }
+    }
+}
